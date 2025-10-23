4b5bcc2f4e3e8db08a7727cf68590220b8e3da48
Fennjoy <113505005+Fennjoy@users.noreply.github.com>
2025-10-23 12:51:13 +0530

    Delete frontend directory

--- frontend/src/components/Footer.js
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-    return  <footer className="py-1 bg-dark">
-                <p className="text-center text-white mt-1 ">
-                    Fennjoy - 2023-2026, All Rights Reserved
-                </p>
-            </footer>
-}
